ba1858abcfca844e2ee66d6b359545b8291cb599
javidnikoo <42614528+javidnikoo@users.noreply.github.com>
2021-02-10 21:47:18 +0100

    basic

---
+++ Uebung-001/main.py
@@ -0,0 +1,15 @@
+def conv(number):
+    ones = ["Null","eins","zwei","drei","vier","fünf","sechs","sieben","acht","neun","zehn","elf","zwölf","dreizehn","vierzehn","fünfzehn","sechzehn","siebzehn","achtzehn","nuenzehn"]
+    tens = ["zwanzig","dreißig","vierzig","fünfzig","sechzig","siebzig","achtzig","neunzig"]
+    if number < 0 or number > 9999999:
+        print("The number is out of the range ")
+    else:
+        
+
+
+
+
+
+
+zahl=int(input("please write your number"))
+conv(zahl)
