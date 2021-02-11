251bdb29cadfbf041407978f1bfa7b8edd207763
javidnikoo <42614528+javidnikoo@users.noreply.github.com>
2021-02-11 17:53:54 +0100

    Funktioniert bis 9999

--- Uebung-001/main.py
+++ Uebung-001/main.py
@@ -1,12 +1,34 @@
+   
 def conv(number):
-    ones = ["Null","eins","zwei","drei","vier","fünf","sechs","sieben","acht","neun","zehn","elf","zwölf","dreizehn","vierzehn","fünfzehn","sechzehn","siebzehn","achtzehn","nuenzehn"]
-    tens = ["zwanzig","dreißig","vierzig","fünfzig","sechzig","siebzig","achtzig","neunzig"]
-    if number < 0 or number > 9999999:
+    zahlw = ["Null","ein","zwei","drei","vier","fünf","sechs","sieben","acht","neun","zehn","elf","zwölf","dreizehn","vierzehn","fünfzehn","sechzehn","siebzehn","achtzehn","nuenzehn"]
+    zehner = ["","zehn","zwanzig","dreißig","vierzig","fünfzig","sechzig","siebzig","achtzig","neunzig"]
+    if number < 0 or number > 9999:
         print("The number is out of the range ")
     else:
-        
+        T = int(number / 1000)
+        H = int(number / 100)
+        H = int( H % 10)
+        Z = int(number / 10)
+        Z = int( Z % 10)
+        E = number % 10
 
-
+        wort=""
+        if (T):
+            wort = zahlw[T] + "tausend"
+        if (H):
+            wort = wort + zahlw[H] + "hundert"
+        if (Z == 0):
+            wort = wort + zahlw[E]
+        elif  Z == 1:
+            Z =  number % 100
+            wort = wort + zahlw[Z]
+        elif (E == 0):
+            wort = wort + zehner[Z]          
+        elif(Z):
+            wort = wort + zahlw[E] + "und" + zahlw[Z] + "zig"
+        if  not T and not H and not Z  and E == 1 :
+            wort="eins" 
+    print (wort)
 
 
 
